bae7f26d65fb1b8094c53744a82c681f3a741757
leonawong <leonawong@digixedu.com>
2023-06-03 01:59:36 +0800

    update header

--- src/components/header.js
+++ src/components/header.js
@@ -7,11 +7,11 @@ export default () => {
   return (
     <>
       <header className="sticky top-0 z-50">
-        <nav className="container flex mx-auto py-3">
+        <nav className="container flex mx-auto py-5 items-center">
             <div id="logo" className="flex">
               <Link className="logo" to="/" />
             </div>
-            <div id="menu" className="flex lowercase">
+            <div id="menu" className="flex items-center">
               <Link className="px-3" to={ROUTES.ABOUT}>about</Link>
               <Link className="px-3" to={ROUTES.PROGRAMS}>programs</Link>
               <Link className="px-3" to={ROUTES.TOURS}>tours</Link>
