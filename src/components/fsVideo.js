56523513694c9ae908e4151e134f294463c202d8
leonawong <leonawong@digixedu.com>
2023-06-06 20:37:18 +0800

    structure + getData updates

---
+++ src/components/fsVideo.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default () => {
+
+  return (
+    <>
+      <div className="bg-video">
+        <video autoPlay loop muted className="w-auto min-w-full min-h-full max-w-none">
+          <source
+            src="https://css-tricks-post-videos.s3.us-east-1.amazonaws.com/Island%20-%204141.mp4"
+            type="video/mp4"
+          />
+        </video>
+      </div>
+      <div className="relative text-center text-white text-bold text-5xl mt-20">
+        <div className="headline">學習航天科技發展 <br />
+        模擬祝融號火星探索</div>
+      </div>
+    </>
+  );
+};
