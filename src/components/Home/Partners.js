f1113faf37a9a810f90e22034e87bf0b3085feb7
leonawong <leonawong@digixedu.com>
2023-10-04 18:29:00 +0800

    update partner & button

--- src/components/Home/Partners.js
+++ src/components/Home/Partners.js
@@ -12,7 +12,7 @@ const Partners = () => {
           主辦機構
         </div>
       </div>
-      <div className="grid items-center mt-8 px-30 grid-cols-2 gap-x-4 gap-y-10 px-8 md:grid-cols-4 md:gap-x-4 md:gap-y-12 lg:grid-cols-4 lg:gap-x-8 lg:gap-y-12 xl:px-40">
+      <div className="grid items-center mt-8 px-30 grid-cols-2 gap-x-4 gap-y-10 px-8 md:grid-cols-3 md:gap-x-4 md:gap-y-12 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-12 xl:px-40">
         <img src="/assets/logo_dxne_h_rgb.svg" alt="digixedu" className="h-auto align-middle border-none justify-self-center" width="220" />
         <img src="/assets/logo_glac.jpg" alt="GLAC" className="h-auto align-middle border-none justify-self-center" width="180" />
         <img src="/assets/logo_nam-yue-research.png" alt="南粵研究院" className="h-auto align-middle border-none justify-self-center" width="120" />
